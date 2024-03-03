bab2d6b75b721731834927e4cc2742140c2ee5f3
Vsanotskyi <vsanotskyi@gmail.com>
2024-03-04 00:43:32 +0200

    lesson 7

---
+++ lesson_7/routes/api/books.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/books");
+
+const {validateBody, isValidId} = require("../../middlewares");
+
+const {schemas} = require("../../models/book");
+
+const router = express.Router();
+
+router.get("/", ctrl.getAll);
+
+router.get("/:id", isValidId, ctrl.getById);
+
+router.post("/", ctrl.add);
+
+router.put("/:id", isValidId, validateBody(schemas.updateSchemas), ctrl.updateById);
+
+router.delete("/:id", isValidId, ctrl.deleteById);
+
+module.exports = router;
